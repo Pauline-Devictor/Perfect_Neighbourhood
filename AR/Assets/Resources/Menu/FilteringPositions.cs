e1013b28cf50f33f33dcd91e3cbade930ec12051
hebabz <heba.bouzidi@etu.unice.fr>
2024-01-09 13:37:37 +0100

    light adds

--- AR/Assets/Resources/Menu/FilteringPositions.cs
+++ AR/Assets/Resources/Menu/FilteringPositions.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FilteringPositions : MonoBehaviour
 {
     //TODO: once a time-slot button is clicked, filter the positions of the objects and suspects in the scene
-    //And the button changes color to indicate that it is selected
 
-    
+    public void PickedTimeSlot(GameObject button)
+    {
+        if(button.GetComponent<Image>().color == Color.cyan)
+        {
+            button.GetComponent<Image>().color = Color.white;
+        }
+        else
+        {
+            button.GetComponent<Image>().color = Color.cyan;
+        }
+    }
 }
