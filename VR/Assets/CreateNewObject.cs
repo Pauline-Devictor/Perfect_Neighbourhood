e4736cc226c5cf7740855ba34213d3eba685ba8b
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2024-01-17 12:05:08 +0100

    Les boutons du slider font apparaitre les objets (reste à tester en vr si le slider slide)

--- VR/Assets/CreateNewObject.cs
+++ VR/Assets/CreateNewObject.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreateNewObject : MonoBehaviour
 {
     [SerializeField] GameObject itemToSpawn;
     [SerializeField] Transform spawningTransform;
+    private float count = 0; // bc isOn always return true even if false
 
     public void CreateNew()
     {
-        Debug.Log("coucou");
-        if (itemToSpawn && spawningTransform)
+        count++;
+        if (itemToSpawn && spawningTransform && count%2 == 1)
+        {
             Instantiate(itemToSpawn, spawningTransform);
+
+        }
+
     }
 }
